b502bb969888625f58d83388a1a55018394ba01b
Kevin Upton <k_upton@live.com>
2015-10-30 22:51:23 +1000

    added serializable features

--- src/Kevupton/Processor/Package.php
+++ src/Kevupton/Processor/Package.php
@@ -109,4 +109,30 @@ abstract class Package implements Serializable {
     public function __toString() {
         return $this->toJSON();
     }
+
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * String representation of object
+     * @link http://php.net/manual/en/serializable.serialize.php
+     * @return string the string representation of the object or null
+     */
+    public function serialize()
+    {
+        return serialize($this);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Constructs the object
+     * @link http://php.net/manual/en/serializable.unserialize.php
+     * @param string $serialized <p>
+     * The string representation of the object.
+     * </p>
+     * @return void
+     */
+    public function unserialize($serialized)
+    {
+        return unserialize($serialized);
+    }
 }
