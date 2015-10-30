4ef96186b5ed4f67335be292f28f7a7d8193b61d
Kevin Upton <k_upton@live.com>
2015-10-31 01:25:23 +1000

    updated the package serialization

--- src/Kevupton/Processor/Package.php
+++ src/Kevupton/Processor/Package.php
@@ -1,12 +1,16 @@
 <?php namespace Kevupton\Processor;
 
+use Illuminate\Queue\SerializesModels;
 use Kevupton\Processor\Exceptions\ProcessException;
 use Kevupton\Processor\Validator\ProcessValidator;
+use ReflectionClass;
 use Serializable;
 use stdClass;
 
 abstract class Package implements Serializable {
     protected $validation = array();
+
+    use SerializesModels;
 
     /**
      * Validates the class requirements with the input data.
@@ -107,31 +111,5 @@ abstract class Package implements Serializable {
      */
     public function __toString() {
         return $this->toJSON();
-    }
-
-
-    /**
-     * (PHP 5 &gt;= 5.1.0)<br/>
-     * String representation of object
-     * @link http://php.net/manual/en/serializable.serialize.php
-     * @return string the string representation of the object or null
-     */
-    public function serialize()
-    {
-        return serialize($this);
-    }
-
-    /**
-     * (PHP 5 &gt;= 5.1.0)<br/>
-     * Constructs the object
-     * @link http://php.net/manual/en/serializable.unserialize.php
-     * @param string $serialized <p>
-     * The string representation of the object.
-     * </p>
-     * @return void
-     */
-    public function unserialize($serialized)
-    {
-        return unserialize($serialized);
     }
 }
